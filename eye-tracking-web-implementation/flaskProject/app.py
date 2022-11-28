4779527c3d2e0846dfe13766360279379c618ae2
Efthymis <efthymis@softweb.gr>
2022-11-29 01:23:39 +0200

    added upload image feature

--- eye-tracking-web-implementation/flaskProject/app.py
+++ eye-tracking-web-implementation/flaskProject/app.py
@@ -1,6 +1,8 @@
+import os
 
-from flask import Flask, request, render_template, Response, jsonify, redirect, url_for
+from flask import Flask, request, render_template, Response, jsonify, redirect, url_for, flash
 from numpy import random
+from werkzeug.utils import secure_filename
 
 import frame_processing
 from camera import VideoCamera
@@ -10,11 +12,20 @@ screen_size_in_inches = 0
 window = []
 number = 0
 show_camera = True
+uploaded_file_name = ""
+UPLOAD_FOLDER = 'C:\\Users\\themi\\Desktop\\Diplomatic\\Repository\\eye-tracking-thesis\\eye-tracking-web-implementation\\flaskProject\\static\\images\\'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 # @app.route('/')
 # def hello_world():  # put application's code here
 #     return render_template("home-page.html")
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def home():
@@ -23,17 +34,41 @@ def home():
 
 @app.route('/verify', methods=['GET', 'POST'])
 def verify():
-    global screen_size_in_inches, window
+    global screen_size_in_inches, window, uploaded_file_name
     if request.method == 'POST':
         inches_input = int(request.form['number'])
         res_input = request.form['Resolution']
         window = [int(res_input.split('×')[0]), int(res_input.split('×')[1])]
-        if isinstance(inches_input, int):
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            # flash('No file part')
+            # return redirect(request.url)
+            return jsonify(isError=True,
+                           message="No file part",
+                           statusCode=400), 400
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            # flash('No selected file')
+            # return redirect(request.url)
+            return jsonify(isError=True,
+                           message="No file part",
+                           statusCode=400), 400
+        # if file and allowed_file(file.filename):
+        #     filename = secure_filename(file.filename)
+        #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #     return redirect(url_for('download_file', name=filename))
+        if isinstance(inches_input, int) and file and allowed_file(file.filename):
             screen_size_in_inches = inches_input
+            filename = secure_filename(file.filename)
+            uploaded_file_name = filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return jsonify(isError=False,
                            statusCode=200), 200
         else:
             screen_size_in_inches = 0
+            uploaded_file_name = ""
             return jsonify(isError=True,
                            message="Please enter integer value",
                            statusCode=400), 400
@@ -54,7 +89,7 @@ def recording():
     frame_processing.state_values.recording_happening = not frame_processing.state_values.recording_happening
     frame_processing.start_recording_to_file()
     number = random.randint(1, 5)
-    return render_template('Recording.html', rand_img=str(number)+'.jpg', width=window[0], height=window[1],
+    return render_template('Recording.html', rand_img=uploaded_file_name, width=window[0], height=window[1],
                            camera_margin=0 if show_camera else window[0])
 
 
@@ -62,8 +97,8 @@ def recording():
 def result():
     frame_processing.state_values.recording_happening = not frame_processing.state_values.recording_happening
     frame_processing.stop_recording_to_file()
-    return render_template('Result.html', heatmap=frame_processing.calculated_values.last_file_name+'.png',
-                           rand_img=str(number)+'.jpg', width=window[0], height=window[1])
+    return render_template('Result.html', heatmap=frame_processing.calculated_values.last_file_name + '.png',
+                           rand_img=uploaded_file_name, width=window[0], height=window[1])
 
 
 @app.route('/1')
